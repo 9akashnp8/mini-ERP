cae4654cb5ca201c18dc8b040aa644cb5619a657
Akash NP <91617769+9akashnp8@users.noreply.github.com>
2022-01-18 07:37:00 +0530

    add tables for laptop management, add custom hardware id generating feature

--- hardware/admin.py
+++ hardware/admin.py
@@ -1,13 +1,20 @@
-from csv import list_dialects
 from django.contrib import admin
 from simple_history.admin import SimpleHistoryAdmin
 from .models import *
+
+class HardwareAdmin(admin.ModelAdmin):
+    list_display = ('id', 'hardware_id', 'emp_id')
 
 admin.site.register(Employee, SimpleHistoryAdmin)
 admin.site.register(Department)
 admin.site.register(Designation)
 admin.site.register(Location)
 admin.site.register(User)
+admin.site.register(Hardware, HardwareAdmin)
+admin.site.register(LaptopBrand)
+admin.site.register(LaptopModel)
+admin.site.register(LaptopMedia)
+admin.site.register(Laptop)
 
 
 
--- hardware/models.py
+++ hardware/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 from simple_history.models import HistoricalRecords
+from django.db.models import Max
 
-# Create your models here.
-
+#Models for Employee side of the app
 class Department(models.Model):
     department_id = models.AutoField(primary_key=True, editable=False)
     dept_name = models.CharField(max_length=50, null=False, blank=False)
@@ -58,3 +58,56 @@ class Employee(models.Model):
 
     def __str__(self):
         return self.emp_name
+
+#Models for the Laptop side of the app
+class LaptopBrand(models.Model):
+    id = models.AutoField(primary_key=True, editable=False)
+    brand_name = models.CharField(max_length=20, null=True, blank=True)
+
+    def __str__(self):
+        return self.brand_name
+
+class LaptopModel(models.Model):
+    id = models.AutoField(primary_key=True, editable=False)
+    brand_id = models.ForeignKey(LaptopBrand, null=True, on_delete=models.SET_NULL)
+    model_name = models.CharField(max_length=50, null=True, blank=True)
+
+    def __str__(self):
+        return self.model_name
+
+class LaptopMedia(models.Model):
+    id = models.AutoField(primary_key=True, editable=False)
+    media = models.ImageField(upload_to='uploads/')
+
+class Laptop(models.Model):
+    LAPTOP_STATUSES = (
+        ('Working', 'Working'),
+        ('Repair', 'Repair'),
+        ('Replace', 'Replace'),
+    )
+    id = models.AutoField(primary_key=True, editable=False)
+    hardware_id = models.CharField(max_length=50, null=True, default=None, blank=True, unique=True)
+    laptop_sr_no = models.CharField(max_length=100, unique=True)
+    brand = models.ForeignKey(LaptopBrand, null=True, on_delete=models.SET_NULL)
+    model = models.ForeignKey(LaptopModel, null=True, on_delete=models.SET_NULL)
+    media = models.ForeignKey(LaptopMedia, null=True, on_delete=models.SET_NULL)
+    laptop_status = models.CharField(max_length=20, null=True, choices=LAPTOP_STATUSES)
+    laptop_date_purchased = models.DateField(null=True)
+    laptop_date_sold = models.DateField(null=True, blank=True)
+    laptop_date_created = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.hardware_id
+
+    def save(self,*args, **kwargs):   
+        if not self.hardware_id:
+            prefix = 'LAK-LAP'
+            id = Laptop.objects.count()+1
+            self.hardware_id = prefix+'-'+'{0:04d}'.format(id)
+        super(Laptop, self).save(*args, **kwargs)
+
+#Model linking the Employee to the various hardwares (Eg: Laptops, Tablets)
+class Hardware(models.Model):
+    id = models.AutoField(primary_key=True, editable=False)
+    emp_id = models.ForeignKey(Employee, null=True, on_delete=models.SET_NULL)
+    hardware_id = models.ForeignKey(Laptop, null=True, on_delete=models.SET_NULL)
