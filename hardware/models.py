2e0f44a3d91ac8ccd03c0a260b0b67e5669a1a6c
Akash NP <akashnp1998@gmail.com>
2022-03-08 16:31:42 +0530

    Add table to laptop_history_page
    with this change, one can track the laptop via images. Via a tabular
    format, one can see the date as well as the corresponsing image of that
    particular laptop.

--- hardware/models.py
+++ hardware/models.py
@@ -145,6 +145,7 @@ class LaptopMedia(models.Model):
     id = models.AutoField(primary_key=True, editable=False)
     media = models.ImageField(upload_to=path_and_rename)
     laptop_id = models.ForeignKey(Laptop, null=True, on_delete=models.CASCADE)
+    uploaded_at = models.DateField(auto_now_add=True)
 
 #Model linking the Employee to the various hardwares (Eg: Laptops, Tablets)
 class Hardware(models.Model):
--- minierp/urls.py
+++ minierp/urls.py
@@ -22,4 +22,4 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('hardware.urls')),
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
