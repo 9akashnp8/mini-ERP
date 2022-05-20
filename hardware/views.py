2ee8c1124e28b7940df30c2dd50319a4af743766
Akash NP <akashnp1998@gmail.com>
2022-05-20 06:16:51 +0530

    Fix Employee Exit form with todays date as exit form's initial value, Add 'exit success' message post processing exit of user

--- hardware/views.py
+++ hardware/views.py
@@ -364,17 +364,19 @@ def emp_exit_confirm(request, pk):
     laptop_assigned = Laptop.objects.get(emp_id=pk)
     hardware_type = Hardware._meta.get_field('hardware_id').remote_field.model.__name__
 
-    laptop_exit_form = EmployeeExitFormLaptop(initial={'laptop_date_returned':'YYYY-DD-MM', 'laptop_return_remarks':''})
+    today = date.today()
+
+    laptop_exit_form = EmployeeExitFormLaptop(initial={'laptop_date_returned':today.strftime('%b %d, %Y'), 'laptop_return_remarks':''})
     laptop_exit_media_form = EmployeeExitFormLaptopImage(instance=laptop_assigned)
 
     if request.method == "POST":
-        laptop_exit_form = EmployeeExitFormLaptop(request.POST, initial={'laptop_date_returned':datetime.date.today(), 'laptop_return_remarks':'Enter Any Remarks here'}, instance=laptop_assigned)
+        laptop_exit_form = EmployeeExitFormLaptop(request.POST, initial={'laptop_date_returned':today.strftime('%b %d, %Y'), 'laptop_return_remarks':'Enter Any Remarks here'}, instance=laptop_assigned)
         laptop_exit_media_form = EmployeeExitFormLaptopImage(request.POST, request.FILES)
         if laptop_exit_form.is_valid() and laptop_exit_media_form.is_valid():
             laptop_assigned.media = laptop_exit_media_form.save()
             laptop_exit_form.save()
             laptop_exit_media_form.save()
-            
+            messages.success(request, f"{employee_info.emp_name}'s Exit succesfully processed", extra_tags="exit_confirm")
             return redirect('emp_exit_complete', employee_info.emp_id)
 
     context = {'employee_info':employee_info, 'hardware_type':hardware_type,
