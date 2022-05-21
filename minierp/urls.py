741a921524b62e5c4f9eadf4c69658179d7e179b
Akash NP <akashnp1998@gmail.com>
2022-05-21 19:24:37 +0530

    reconfigure home url to '' instead of /hardware/

--- minierp/urls.py
+++ minierp/urls.py
@@ -21,5 +21,5 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hardware/', include('hardware.urls')),
+    path('', include('hardware.urls')),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
