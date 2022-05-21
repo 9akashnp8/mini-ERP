87683ab95ec8d6d227a1f396f45e1e8ab6dc66df
Akash NP <akashnp1998@gmail.com>
2022-05-21 19:49:20 +0530

    Fix STATIC_ROOT value

--- minierp/settings.py
+++ minierp/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
-STATIC_ROOT = STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
+STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
