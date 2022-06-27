d8066d26f61da062b6e34ab5c585fb997ecd719e
Akash NP <akashnp1998@gmail.com>
2022-06-27 19:52:47 +0530

    Add Import Export for Designations

--- hardware/admin.py
+++ hardware/admin.py
@@ -4,16 +4,23 @@ from import_export.admin import ImportExportModelAdmin
 from simple_history.admin import SimpleHistoryAdmin
 from .models import *
 
+#Resources for Import-Export library
 class LaptopResource(resources.ModelResource):
     class Meta:
         model = Laptop
 
 class EmployeeResource(resources.ModelResource):
-    
     class Meta:
         model = Employee
         import_id_fields = ('emp_id',)
+    
+class DesignationResource(resources.ModelResource):
+    class Meta:
+        model = Designation
+        exclude = ('id')
+        import_id_fields = ('designation_id',)
 
+#Import Export classes for Admin page integration
 class HardwareAdmin(ImportExportModelAdmin):
     list_display = ('id', 'hardware_id', 'emp_id')
 
@@ -23,10 +30,13 @@ class EmployeeAdmin(ImportExportModelAdmin):
 class LaptopModelAdmin(ImportExportModelAdmin):
     resource_class = LaptopResource
 
+class DesignationModelAdmin(ImportExportModelAdmin):
+    resource_class = DesignationResource
+
 
 admin.site.register(Employee, EmployeeAdmin)
 admin.site.register(Department)
-admin.site.register(Designation)
+admin.site.register(Designation, DesignationModelAdmin)
 admin.site.register(Location)
 admin.site.register(Hardware, HardwareAdmin)
 admin.site.register(LaptopBrand)
