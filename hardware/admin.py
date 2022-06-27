25d1366f263c21a1519400a0b85942bf3f4804f6
Akash NP <akashnp1998@gmail.com>
2022-06-27 15:51:28 +0530

    Fix Import & Export usage

--- hardware/admin.py
+++ hardware/admin.py
@@ -1,14 +1,28 @@
 from django.contrib import admin
+from import_export import resources
 from import_export.admin import ImportExportModelAdmin
 from simple_history.admin import SimpleHistoryAdmin
 from .models import *
 
+class LaptopResource(resources.ModelResource):
+    class Meta:
+        model = Laptop
+
+class EmployeeResource(resources.ModelResource):
+    
+    class Meta:
+        model = Employee
+        import_id_fields = ('emp_id',)
+
 class HardwareAdmin(ImportExportModelAdmin):
     list_display = ('id', 'hardware_id', 'emp_id')
-    pass
 
 class EmployeeAdmin(ImportExportModelAdmin):
-    pass
+    resource_class = EmployeeResource
+
+class LaptopModelAdmin(ImportExportModelAdmin):
+    resource_class = LaptopResource
+
 
 admin.site.register(Employee, EmployeeAdmin)
 admin.site.register(Department)
@@ -18,7 +32,7 @@ admin.site.register(Hardware, HardwareAdmin)
 admin.site.register(LaptopBrand)
 admin.site.register(LaptopModel)
 admin.site.register(LaptopMedia)
-admin.site.register(Laptop, SimpleHistoryAdmin)
+admin.site.register(Laptop, LaptopModelAdmin)
 
 
 
