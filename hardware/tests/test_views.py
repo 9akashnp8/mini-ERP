bf3cf0c8b23e641ffaebdd2ff7cb22bfa3c4e69a
Akash NP <91617769+9akashnp8@users.noreply.github.com>
2022-08-05 08:08:47 +0530

    Add Basic Tests & Set up Github Actions to run tests (#2)
    * Remove unused onboarding view & url
    
    * Add Basic Tests for all 'Employee' related views & urls
    
    Basic tests involving 1. URL Exists, 2. URL by name exists & 3. Correct
    template used have been added. These test even though basic, will give a
    foundation for setting up a CI/CD Pipeline with Github
    
    * Give order to the position of the various views for better clarity
    
    * Set up Github Actions yaml file
    
    Configured the actions to run python manage.py test, this gives us a very
    basic CI pipline where tests are run evertime code is pushed to main
    or a pull request is created to main.
    
    This will later be coupled with CD as well for automatic pushing of
    code to our heroku server
    
    * Update  Github Actions with Correct python version
    
    * Update  Github Actions with Correct python version
    
    * Update yaml with creation of .env file with values as Github Repo Secrets

--- hardware/tests/test_views.py
+++ hardware/tests/test_views.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.models import User, Group
-from hardware.models import Employee
+from hardware.models import Department, Designation, Employee, Location
 from django.test import SimpleTestCase, TestCase
 from django.test.client import Client
 from django.urls import reverse
+from hardware.views import *
 
 class LoginTestCase(TestCase):
 
@@ -13,10 +14,6 @@ class LoginTestCase(TestCase):
         self.user = User.objects.create_user(username='test-user', email='test@test.com', password='test123')
         self.user.groups.add(Group.objects.get(name='admin'))
         self.client.login(username='test-user', password='test123')
-    
-    def tearDown(self):
-        self.user.delete()
-        self.group.delete()
 
 class HomePageTest(LoginTestCase):
 
@@ -32,19 +29,142 @@ class HomePageTest(LoginTestCase):
         response = self.client.get(reverse('home'))
         self.assertTemplateUsed(response, 'dashboard.html')
 
-class OnboardingStep1PageTests(LoginTestCase):
+class EmployeeDashboardPageTests(LoginTestCase):
 
     def test_url_exists_at_correct_location(self):
-        response = self.client.get('/onboard/add/')
+        response = self.client.get("/employees/")
         self.assertEqual(response.status_code, 200)
     
     def test_url_available_by_name(self):
-        response = self.client.get(reverse('onbrd_emp_add'))
+        response = self.client.get(reverse('dash_employees'))
         self.assertEqual(response.status_code, 200)
     
     def test_correct_template_being_used(self):
-        response = self.client.get(reverse('onbrd_emp_add'))
-        self.assertTemplateUsed(response, 'onboard/onboarding_add_employee.html')
+        response = self.client.get(reverse('dash_employees'))
+        self.assertTemplateUsed(response, 'employees/employees.html')
+
+class EmployeeDetailPageTests(LoginTestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+
+        cls.group = Group.objects.create(name="employee")
+        cls.employee = Employee.objects.create(
+            lk_emp_id = 'LA-IND-X',
+            dept_id = Department.objects.create(dept_name='Test Dept'),
+            desig_id = Designation.objects.create(
+            dept_id=Department.objects.get(dept_name='Test Dept'), 
+            designation="Test Desig"), 
+            emp_name = "Akash Test",
+            emp_email = "akash.np@lakshyaca.com",
+            emp_phone = "8075680388",
+            emp_status = "Active",
+            loc_id = Location.objects.create(location="Test Location"),
+            emp_date_joined = "2022-08-03",
+        )
+
+    def test_url_exists_at_correct_location(self):
+        url = reverse(employee, args=(self.employee.emp_id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+    
+    def test_url_available_by_name(self):
+        url = reverse('employee', args=(self.employee.emp_id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+    
+    def test_correct_template_being_used(self):
+        url = reverse('employee', args=(self.employee.emp_id,))
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'employees/employee.html')
+
+class EmployeeAddPageTests(LoginTestCase):
+
+    def test_url_exists_at_correct_location(self):
+        url = reverse(employee_add_view)
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+    
+    def test_url_available_by_name(self):
+        url = reverse('employee_add')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+    
+    def test_correct_template_being_used(self):
+        url = reverse('employee_add')
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'employees/add_new_employee.html')
+
+class EmployeeEditPageTests(LoginTestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+
+        cls.group = Group.objects.create(name="employee")
+        cls.employee = Employee.objects.create(
+            lk_emp_id = 'LA-IND-X',
+            dept_id = Department.objects.create(dept_name='Test Dept'),
+            desig_id = Designation.objects.create(
+            dept_id=Department.objects.get(dept_name='Test Dept'), 
+            designation="Test Desig"), 
+            emp_name = "Akash Test",
+            emp_email = "akash.np@lakshyaca.com",
+            emp_phone = "8075680388",
+            emp_status = "Active",
+            loc_id = Location.objects.create(location="Test Location"),
+            emp_date_joined = "2022-08-03",
+        )
+
+    def test_url_exists_at_correct_location(self):
+        url = reverse(employee_edit_view, args=(self.employee.emp_id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+    
+    def test_url_available_by_name(self):
+        url = reverse('employee_edit', args=(self.employee.emp_id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+    
+    def test_correct_template_being_used(self):
+        url = reverse('employee_edit', args=(self.employee.emp_id,))
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'employees/add_new_employee.html')
+
+class EmployeeDeletePageTests(LoginTestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+
+        cls.group = Group.objects.create(name="employee")
+        cls.employee = Employee.objects.create(
+            lk_emp_id = 'LA-IND-X',
+            dept_id = Department.objects.create(dept_name='Test Dept'),
+            desig_id = Designation.objects.create(
+            dept_id=Department.objects.get(dept_name='Test Dept'), 
+            designation="Test Desig"), 
+            emp_name = "Akash Test",
+            emp_email = "akash.np@lakshyaca.com",
+            emp_phone = "8075680388",
+            emp_status = "Active",
+            loc_id = Location.objects.create(location="Test Location"),
+            emp_date_joined = "2022-08-03",
+        )
+
+    def test_url_exists_at_correct_location(self):
+        url = reverse(employee_delete_view, args=(self.employee.emp_id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+    
+    def test_url_available_by_name(self):
+        url = reverse('employee_del', args=(self.employee.emp_id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+    
+    def test_correct_template_being_used(self):
+        url = reverse('employee_del', args=(self.employee.emp_id,))
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'employees/employee_delete_form.html')
+
 
 # class OnboardingStep2PageTests(LoginTestCase):
 
