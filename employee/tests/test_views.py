82c988d8e718433a1ef4d92855aa29f51cd83666
Akash NP <akashnp1998@gmail.com>
2022-08-15 20:33:14 +0530

    Move tests directory to employee app

--- employee/tests/test_views.py
+++ employee/tests/test_views.py
@@ -1,9 +1,9 @@
 from django.contrib.auth.models import User, Group
-from hardware.models import Department, Designation, Employee, Location
-from django.test import SimpleTestCase, TestCase
+from employee.models import Department, Designation, Employee, Location
+from django.test import TestCase
 from django.test.client import Client
 from django.urls import reverse
-from hardware.views import *
+from employee.views import *
 
 class LoginTestCase(TestCase):
 
@@ -164,22 +164,3 @@ class EmployeeDeletePageTests(LoginTestCase):
         url = reverse('employee_del', args=(self.employee.emp_id,))
         response = self.client.get(url)
         self.assertTemplateUsed(response, 'employees/employee_delete_form.html')
-
-
-# class OnboardingStep2PageTests(LoginTestCase):
-
-#     def test_url_exists_at_correct_location(self):
-#         response = self.client.get(f'/onboard/assign/{self.employee.emp_id}/')
-#         self.assertEqual(response.status_code, 200)
-    
-    # def test_url_available_by_name_and_valid_employee(self):
-    #     print(self.user.id)
-    #     response = self.client.get(reverse('onbrd_hw_assign', args=[182]))
-    #     self.assertEqual(response.status_code, 200)
-    
-    # def test_url_available_by_name_with_invalid_employee(self):
-    #     pass
-    
-    # def test_correct_template_being_used(self):
-    #     response = self.client.get(reverse('onbrd_hw_assign', args=[182]))
-    #     self.assertTemplateUsed(response, 'onboard/onboarding_assign_hardware.html')
