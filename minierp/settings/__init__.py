8130f9527c5e4858f38b4c912154e2b33cc85255
Akash NP <akashnp1998@gmail.com>
2022-09-07 12:46:00 +0530

    Update settings/__init__ to check environment to load settings

--- minierp/settings/__init__.py
+++ minierp/settings/__init__.py
@@ -1,1 +1,10 @@
-from .dev import *
+from environs import Env
+env = Env()
+env.read_env()
+
+ENVIRONMENT = env.str("ENVIRONMENT")
+
+if ENVIRONMENT == 'production':
+    from .production import *
+else:
+    from .dev import *
