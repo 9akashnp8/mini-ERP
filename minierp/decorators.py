b0396bf2cefc2aac921415cdaab2ed94e480f583
Akash NP <akashnp1998@gmail.com>
2022-09-18 09:14:38 +0530

    Comment our unused decorators

--- minierp/decorators.py
+++ minierp/decorators.py
@@ -9,29 +9,29 @@ def unauthenticated_user(view_func):
             return view_func(request, *args, **kwargs)
     return wrapper_func
 
-def allowed_users(allowed_roles=[]):
-    def decorator(view_func):
-        def wrapper_func(request, *args, **kwargs):
-            group = None
-            if request.user.groups.exists():
-                group = request.user.groups.all()[0].name
-            
-            if group in allowed_roles:
-                return view_func(request, *args, **kwargs)
-            else:
-                return HttpResponse("You are not authorized to view this page!")
-        return wrapper_func
-    return decorator
+# def admin_only(view_func):
+#     def wrapper_func(request, *args, **kwargs):
+#         group = None
+#         if request.user.groups.exists():
+#             group = request.user.groups.all()[0].name
+        
+#         if group == 'employee':
+#             return redirect('empprofile')
+        
+#         if group == 'admin':
+#             return view_func(request, *args, **kwargs)
+#     return wrapper_func
 
-def admin_only(view_func):
-    def wrapper_func(request, *args, **kwargs):
-        group = None
-        if request.user.groups.exists():
-            group = request.user.groups.all()[0].name
-        
-        if group == 'employee':
-            return redirect('empprofile')
-        
-        if group == 'admin':
-            return view_func(request, *args, **kwargs)
-    return wrapper_func
+# def allowed_users(allowed_roles=[]):
+#     def decorator(view_func):
+#         def wrapper_func(request, *args, **kwargs):
+#             group = None
+#             if request.user.groups.exists():
+#                 group = request.user.groups.all()[0].name
+            
+#             if group in allowed_roles:
+#                 return view_func(request, *args, **kwargs)
+#             else:
+#                 return HttpResponse("You are not authorized to view this page! Login with an user account authorized to view this page.")
+#         return wrapper_func
+#     return decorator
