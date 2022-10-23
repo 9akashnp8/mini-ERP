98d0b74c27eb2a4c1bd75e41a98153c9f7340140
Akash NP <akashnp1998@gmail.com>
2022-10-23 21:01:26 +0530

    Add API Endpoint for Payments
    Add Serializer, Viewset & Route for the Payment model.

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from .views import *
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
@@ -10,12 +11,17 @@ router.register(r'department', DepartmentViewSet, basename='department')
 router.register(r'designation', DesignationViewSet, basename='designation')
 router.register(r'location', LocationViewSet, basename='location')
 
-#Hardware routes
+# Hardware routes
 router.register(r'laptop', LaptopViewSet, basename='laptop')
 router.register(r'laptop-brand', LaptopBrandViewSet, basename='laptop-brand')
 router.register(r'building', BuildingViewSet, basename='building')
 
+# Finance routes
+router.register(r'payment', PaymentViewSet, basename='payment')
+
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
