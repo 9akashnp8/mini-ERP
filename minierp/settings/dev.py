2c4f0ad00eb569c31889c1bbfe80fcb156627fd9
Akash NP <akashnp1998@gmail.com>
2022-11-03 23:19:29 +0530

    Update dev settings to be able to switch b/w db engines based on .env value

--- minierp/settings/dev.py
+++ minierp/settings/dev.py
@@ -4,11 +4,23 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
-DATABASES = {
-    'default': env.dj_db_url("DATABASE_URL")
-}
+db_engine = env.str("DB_ENGINE")
+if db_engine == 'sqlite':
+    DATABASES = {
+        'default': env.dj_db_url("DATABASE_URL")
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'postgres',
+            'USER': 'postgres',
+            'PASSWORD': 'postgres',
+            'HOST': 'postgres_db'
+        }
+    }
 
 STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
 STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
 
-CELERY_BROKER_URL = "redis://localhost:6379"
+CELERY_BROKER_URL = "redis://redis:6379"
