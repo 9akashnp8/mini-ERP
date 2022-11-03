b9a6a4fa4cba1a1fedbf8ac60ee951f55c1fd686
Akash NP <91617769+9akashnp8@users.noreply.github.com>
2022-11-03 23:09:55 +0530

    Dynamic Hardware Assignment & Return Form Generation (#15)
    * Add New Feature: Hardware Form Generation for Offline Record Keeping
    
    Written Hardware Forms are not reliable as serial numbers can easily be
    miss written/intepreted (handwritings vary). With this feature, hardware
    forms can dynamically be generated.
    
    Only a  'Ctrl + P' away from reliable offline record keeping.
    
    * Add 'Generate Form' dropdown to Laptop Detail Page
    
    Generate Form dropdown to allow single click generation of the custom
    dynamic hardware assignment and return forms.

--- hardware/urls.py
+++ hardware/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('laptop/<str:pk>/edit/', views.laptop_edit_view, name='laptop_edit'),
     path('laptop/<str:pk>/delete/', views.laptop_delete_view, name='laptop_del'),
     path('laptop-return/<str:pk>/', views.laptop_return, name='replace_confirm'),
+    path('laptop/<str:pk>/form/', views.generate_hardware_form, name='generate_hardware_form'),
 
     path('htmx/load-buildings/', views.load_buildings, name='load_buildings'),
     path('htmx/search-result-assign/', views.search_results_for_laptop_assignment, name='search_results_for_laptop_assignment'),
