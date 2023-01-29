043842aaf61dd5e0a23a54a32eca92dd6fc47bf8
Akash NP <akashnp1998@gmail.com>
2023-01-29 13:25:59 +0530

    chore: add EMAIL_BACKEND to settings

--- minierp/settings/defaults.py
+++ minierp/settings/defaults.py
@@ -134,6 +134,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 #Email Settings:
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
