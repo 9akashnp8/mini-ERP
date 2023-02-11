e785333582181965845e16f0971ca0225a3cc59c
Akash NP <akashnp1998@gmail.com>
2023-02-11 17:29:04 +0530

    fix: missing import

--- api/views/hardware.py
+++ api/views/hardware.py
@@ -4,7 +4,7 @@ from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-from django.db.models import Count
+from django.db.models import Count, Q
 
 from hardware.models import (
     Laptop,
