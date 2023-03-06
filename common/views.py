6dd90806c2ee99443a47d6a6adfd94264806ce3a
Akash NP <akashnp1998@gmail.com>
2023-03-06 19:53:56 +0530

    feat: move all common views, urls and other modules to common app

--- common/views.py
+++ common/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
-# Create your views here.
+from .decorators import unauthenticated_user
+
+@unauthenticated_user
+def login_page(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, "username OR password is Incorrect.")
+
+    context = {}
+    return render(request, 'common/login.html', context)
+
+def logout_page(request):
+    logout(request)
+    return redirect('login')
+
+@login_required(login_url='login')
+def home(request):
+    return render(request, 'dashboard.html')
+
+@login_required(login_url='login')
+def admin_panel(request):
+    return render(request, 'common/admin_panel_home.html')
+
