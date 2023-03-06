80613f1fc81c7a328ca7d8601e9091452599d7fc
Akash NP <91617769+9akashnp8@users.noreply.github.com>
2023-03-06 22:55:12 +0530

    improve 'app setting' link placement, setup 'common' app (#24)
    * feat: Improve 'App Setting' links placement, add to admin-panel home page
    
    * feat: move all common views, urls and other modules to common app
    
    * feat: setup 'app setting' models in common app
    
    add inital object creation as migration to ensure 1 object always exists
    
    * fix: remove initial object creation via migration, stick to load() of BaseSetting model
    
    * feat: set queried settings objects to cache for reduced queries
    
    * feat: Make implementation ready
    
    add blank=True, null=True for not required fields
    add defaults for required fields
    convert normal CharFields to ArrayField to store list of screensizes and
    rental vendors
    
    setting the above ensure the Singleton load() method works as expected
    when calling for the first time (object not created) i.e, it creates the
    first object without any errors (cannot be null etc)
    
    * fix: set default for ArrayField to callable that returns list, as Django has suggessted
    
    * fix: reset migrations

--- common/admin.py
+++ common/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import (
+    EmployeeAppSetting,
+    HardwareAppSetting
+)
+
 # Register your models here.
+admin.site.register(EmployeeAppSetting)
+admin.site.register(HardwareAppSetting)
--- common/models.py
+++ common/models.py
@@ -1,4 +1,10 @@
 from django.db import models
+from django.core.cache import cache
+from django.contrib.postgres.fields import ArrayField
+
+# Helpers
+def get_default_screen_sizes():
+    return ['14 inch, 15 inch']
 
 # Create your models here.
 class BaseSetting(models.Model):
@@ -9,6 +15,7 @@ class BaseSetting(models.Model):
     def save(self, *args, **kwargs):
         self.pk = 1
         super().save(*args, **kwargs)
+        self.set_cache()
 
     def delete(self, *args, **kwargs):
         """
@@ -19,17 +26,62 @@ class BaseSetting(models.Model):
 
     @classmethod
     def load(cls):
-        obj, _ = cls.objects.get_or_create(pk=1)
-        return obj
+        if cache.get(cls.__name__) is None:
+            obj, created = cls.objects.get_or_create(pk=1)
+            if not created:
+                obj.set_cache()
+        return cache.get(cls.__name__)
+
+    def set_cache(self):
+        cache.set(self.__class__.__name__, self)
 
 class EmployeeAppSetting(BaseSetting):
-    emp_id_prefix = models.CharField(max_length=200)
+    emp_id_prefix = models.CharField(
+        max_length=255,
+        default='MYORG'
+    )
+
+    def __str__(self) -> str:
+        return 'Employee App Setting'
 
 class HardwareAppSetting(BaseSetting):
-    hardware_id_prefix = models.CharField(max_length=200)
-    default_processor = models.CharField(max_length=30)
-    default_ram = models.CharField(max_length=10)
-    default_storage = models.CharField(max_length=10)
-    screen_sizes = models.CharField(max_length=100)
-    rental_vendors = models.CharField(max_length=500)
-    organization_name = models.CharField(max_length=100)
+    hardware_id_prefix = models.CharField(
+        max_length=255,
+        default='LAP'
+    )
+    default_processor = models.CharField(
+        max_length=255,
+        blank=True,
+        null=True
+    )
+    default_ram = models.CharField(
+        max_length=255,
+        blank=True,
+        null=True
+    )
+    default_storage = models.CharField(
+        max_length=255,
+        blank=True,
+        null=True
+    )
+    screen_sizes = models.CharField(
+        max_length=255,
+        blank=True,
+        null=True
+    )
+    screen_sizes = ArrayField(
+        base_field=models.CharField(max_length=255),
+        default=get_default_screen_sizes
+    )
+    rental_vendors = ArrayField(
+        base_field=models.CharField(max_length=255),
+        blank=True,
+        null=True
+    )
+    organization_name = models.CharField(
+        max_length=255,
+        default='My Organization'
+    )
+
+    def __str__(self) -> str:
+        return 'Hardware App Setting'
