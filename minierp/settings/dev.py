0f981565def8207d7e7d3bf402fd1c509b6abf2b
Akash NP <akashnp1998@gmail.com>
2023-06-11 11:43:38 +0530

    chore: flake8 fixes

--- minierp/settings/dev.py
+++ minierp/settings/dev.py
@@ -1,13 +1,13 @@
-from .defaults import *
+from .defaults import *  # noqa: F403
 
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
-db_engine = env.str("DB_ENGINE")
+db_engine = env.str("DB_ENGINE")  # noqa: F405
 if db_engine == 'sqlite':
     DATABASES = {
-        'default': env.dj_db_url("DATABASE_URL")
+        'default': env.dj_db_url("DATABASE_URL")  # noqa: F405
     }
 else:
     DATABASES = {
@@ -16,11 +16,11 @@ else:
             'NAME': 'postgres',
             'USER': 'postgres',
             'PASSWORD': 'postgres',
-            'HOST': 'test_db'
+            'HOST': 'localhost'
         }
     }
 
-STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
-STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
+STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]  # noqa: F405
+STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))  # noqa: F405
 
-CELERY_BROKER_URL = "redis://redis:6379"
+CELERY_BROKER_URL = "redis://localhost:6379"
