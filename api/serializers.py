6d9a5e0146b7f88a2df9cd41398f1c14c2d54c66
Akash NP <akashnp1998@gmail.com>
2023-06-13 21:18:12 +0530

    feat: add user creation of employee to create api
    eventually, remove user creation from signal, having it
    in signal makes it scattered.
    
    add compatible validation error so that FE can handle accordingly

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,6 @@
-from rest_framework import serializers
 from django.contrib.auth.models import User
+from django.db.utils import IntegrityError
+from rest_framework import serializers
 
 from employee.models import Department, Designation, Employee, Location
 from hardware.models import Building, Laptop, LaptopBrand
@@ -63,6 +64,22 @@ class EmployeeCreateSerializer(BaseEmployeeSerializer):
 
     class Meta(BaseEmployeeSerializer.Meta):
         depth = 0
+
+    def create(self, validated_data):
+        """
+        create and link corresponding User object to Employee.
+        """
+        email = validated_data.get('emp_email')
+        try:
+            # move default password to settings
+            user = User.objects.create_user(email, email, 'lakshya123')
+        except IntegrityError:
+            raise serializers.ValidationError({
+                "emp_email": ["User with username/email already exists"]
+            })
+        else:
+            employee = Employee.objects.create(user=user, **validated_data)
+            return employee
 
 
 # Hardware Serializers
