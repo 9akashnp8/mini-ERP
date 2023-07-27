e0f1cc75c4bd3b5f87efca8a1b44bf48d4e8479d
Akash NP <akashnp1998@gmail.com>
2023-07-27 20:48:57 +0400

    fix: update whitenoise storage config as per django 4.2 req

--- minierp/settings/defaults.py
+++ minierp/settings/defaults.py
@@ -124,7 +124,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage"
+    }
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
