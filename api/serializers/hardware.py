2c7d534b64a9d3dc1147576f895b29adbe083f8a
Akash NP <akashnp1998@gmail.com>
2023-12-02 19:01:55 +0400

    feat: add V2 serializers, header based versioning for  viewset

--- api/serializers/hardware.py
+++ api/serializers/hardware.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 
 from hardware.models import (
     Laptop,
+    LaptopV2,
     LaptopBrand,
     Hardware,
     HardwareType,
@@ -48,16 +49,38 @@ class HardwareConditionSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 
-class LaptopSerializer(serializers.ModelSerializer):
+class LaptopV1ListRetrieveSerializer(serializers.ModelSerializer):
     class Meta:
         model = Laptop
         fields = "__all__"
         depth = 1
 
 
-class LaptopCreateSerializer(LaptopSerializer):
-    class Meta(LaptopSerializer.Meta):
+class LaptopV1CreateSerializer(LaptopV1ListRetrieveSerializer):
+    class Meta(LaptopV1ListRetrieveSerializer.Meta):
         depth = 0
+
+
+class LaptopV2CreateSerializer(serializers.ModelSerializer):
+    hardware_id = HardwareCreateUpdateDestroySerializer()
+
+    class Meta:
+        model = LaptopV2
+        exclude = ["id"]
+        extra_kwargs = {"brand": {"required": True}, "screen_size": {"required": True}}
+
+    def create(self, validated_data):
+        hardware_data = validated_data.pop("hardware_id")
+        return LaptopV2.objects.create(hardware_data, validated_data)
+
+
+class LaptopV2ListRetrieveSerializer(serializers.ModelSerializer):
+    hardware_id = HardwareListRetrieveSerializer(read_only=True)
+
+    class Meta:
+        model = LaptopV2
+        exclude = ["id"]
+        depth = 1
 
 
 class LaptopBrandSerializer(serializers.ModelSerializer):
