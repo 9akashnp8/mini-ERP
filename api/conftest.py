3aa240e49597bb1a82bc5f13de7ad295c3212481
Akash NP <akashnp1998@gmail.com>
2023-12-03 13:03:45 +0400

    chore: add tests for updated laptop apis

--- api/conftest.py
+++ api/conftest.py
@@ -7,6 +7,8 @@ from hardware.models import (
     HardwareOwner,
     HardwareCondition,
     Building,
+    LaptopBrand,
+    LaptopScreenSize,
 )
 from employee.models import Location
 
@@ -44,4 +46,16 @@ def seed_employee_location():
 def seed_hardware_building():
     location = Location.objects.create(location="Test Location 2")
     obj = Building.objects.create(location=location, building="Test Building")
+    return obj.id
+
+
+@pytest.fixture(scope="function")
+def seed_laptop_brand():
+    obj = LaptopBrand.objects.create(brand_name="Test Laptop Brand")
+    return obj.id
+
+
+@pytest.fixture(scope="function")
+def seed_laptop_screen_size():
+    obj = LaptopScreenSize.objects.create(size_range='14" - 15"')
     return obj.id
