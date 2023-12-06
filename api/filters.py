36d5303dcc8e3692994605c6be77d94cdfbaec24
Akash NP <akashnp1998@gmail.com>
2023-12-06 23:21:25 +0400

    feat: add filter to list hardware by availablility i.e., free/not
    implement queryset method that contains all logic to retrieve
    free/available hardware. This ensures that logic is centralized
    and easily accessible and chained from any queryset.

--- api/filters.py
+++ api/filters.py
@@ -1,6 +1,20 @@
 from django_filters import rest_framework as filters
 
-from hardware.models import HardwareAssignment
+from hardware.models import HardwareAssignment, Hardware
+
+
+class HardwareFilter(filters.FilterSet):
+    is_free = filters.BooleanFilter(method="filter_is_free")
+    type = filters.NumberFilter(field_name="type")
+
+    def filter_is_free(self, queryset, field_name, value):
+        if value is True:
+            return queryset.get_available_hardware()
+        return queryset
+
+    class Meta:
+        model = Hardware
+        fields = ["type"]
 
 
 class HardwareAssignmentFilter(filters.FilterSet):
